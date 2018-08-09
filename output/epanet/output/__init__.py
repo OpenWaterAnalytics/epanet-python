259bad837e0bcbd60e5d9c9e94202df7390d55f7
Michael Tryby <tryby.michael@epa.gov>
2018-08-09 18:08:26 -0400

    Adding metadata

--- output/epanet/output/__init__.py
+++ output/epanet/output/__init__.py
@@ -1,2 +1,54 @@
 
-from epanet.output import output
+
+
+# Units of Measurement
+#
+# Units                     US Customary             SI Metric
+#   Concentration              mg/L                     mg/L
+#                              ug/L                     ug/L
+#   Demand                     flow                     flow
+#   Diameter                   in                       mm
+#   Efficiency                 percent                  percent
+#   Elevation                  feet                     meters
+#   Emitter Coefficient        flow/(psi)^1/2           flow/(meters)^1/2
+#   Energy                     kilowatt-hours           kilowatt-hours
+#   Flow                       CFS                      LPS
+#                              GPM                      LPM
+#                              MGD                      MLD
+#                              IMGD                     CMH
+#                              AFD                      CMD
+#   Friction Factor            unitless                 unitless
+#   Hydraulic Head             feet                     meters
+#   Length                     feet                     meters
+#   Minor Loss Coefficient     unitless                 unitless
+#   Power                      horsepower               kilowatts
+#   Pressure                   psi                      meters
+#   Reaction Coeff (Bulk)      1/day (1st-order)        1/day
+#   Reaction Coeff (Wall)      mass/L/day (0th-order)   mass/L/day
+#                              ft/day (1st-order)       meters/day (1st-order)
+#   Roughness Coeff            10^-3 feet (DW)          mm (DW)
+#                              unitless                 unitless
+#   Source Mass Injection      mass/min                 mass/min
+#   Velocity                   feet/sec                 meters/sec
+#   Volume                     cubic feet               cubic meters
+#   Water Age                  hours                    hours
+
+
+
+# Output Metadata
+#
+# Node Attributes  Long Name        Units
+#   DEMAND            Demand           
+#   HEAD              Head
+#   PRESSURE          Pressure
+#   QUALITY           Quality
+
+# Link Attributes
+#   FLOW              Flow
+#   VELOCITY          Velocity
+#   HEADLOSS          Unit Headloss
+#   AVG_QUALITY       Quality
+#   STATUS            Status
+#   SETTING           Setting
+#   RX_RATE           Reaction Rate
+#   FRCTN_FCTR        Friction Factor
