ebbba107dad1db6403833e2ce5201cd80d419a6b
Michael Tryby <tryby.michael@epa.gov>
2018-10-19 10:14:29 -0400

    Adding tests

---
+++ toolkit/tests/data/__init__.py
@@ -0,0 +1,9 @@
+
+
+import os
+
+DATA_PATH = os.path.abspath(os.path.dirname(__file__))
+
+INPUT_FILE_NET_1 = os.path.join(DATA_PATH, 'Net1.inp')
+REPORT_FILE_TEST = os.path.join(DATA_PATH, 'test.rpt')
+OUTPUT_FILE_TEST = os.path.join(DATA_PATH, 'test.out')
