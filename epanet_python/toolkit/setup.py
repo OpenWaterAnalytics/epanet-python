94a9e0565de8d1a55b327e66bc93cef67b326bfa
Michael Tryby <tryby.michael@epa.gov>
2019-02-15 15:31:14 -0500

    Updating toolkit setup.py

--- epanet_python/toolkit/setup.py
+++ epanet_python/toolkit/setup.py
@@ -8,36 +8,33 @@
 #
 # Requires:
 #   Platform C language compiler
-#   SWIG
 #
 
-try:
-    from setuptools import setup, Extension
-    from setuptools.command.build_ext import build_ext
-except ImportError:
-    from distutils.core import setup, Extension
-    from distutils.command.build_ext import build_ext
 
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 
-microlib_name = 'epanet.toolkit'
 
 setup(
-    name = microlib_name,
+    name = 'epanet.toolkit',
     version = "0.0.3a0",
     ext_modules = [
         Extension("epanet.toolkit._toolkit",
-            sources = ['epanet/toolkit/toolkit.i'],
-            swig_opts = ['-py3'],
+            sources = ['epanet/toolkit/toolkit_wrap.c'],
             include_dirs = ['epanet/toolkit'],
             library_dirs = ['epanet/toolkit'],
             libraries = ['epanet_py'],
             language = 'C'
         )
     ],
+
+    # tox can't find epanet module at test time unless namespace is declared
     namespace_packages = ['epanet'],
-    packages = {microlib_name},
+
+    packages = {'epanet.toolkit'},
     py_modules = ['toolkit'],
-#    include_package_data=True
-    package_data = {microlib_name:['*epanet_py.dll', '*epanet_py.so']},
+    package_data = {'epanet.toolkit':['*epanet_py.dll', '*epanet_py.so']},
+
+    zip_safe=False
 
 )
