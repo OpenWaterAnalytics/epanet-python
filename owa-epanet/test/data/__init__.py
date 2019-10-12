ed53a9de661ddc561310831276720783c169cf80
Ernesto Arandia <ernesto.arandia@xyleminc.com>
2019-10-12 17:33:25 -0400

    adds api function tests for the swig-wrapped epanet toolkit

---
+++ owa-epanet/test/data/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019 CitiLogics, a Xylem Brand
+#
+# -----------------------------------------------------------------------------
+"""test data for owa-epanet toolkit."""
+
+# Standard library imports
+import os
+
+data_path = os.path.abspath(os.path.dirname(__file__))
+
+# Test object paths
+example_1_path = os.path.join(data_path, 'example_1.inp')
