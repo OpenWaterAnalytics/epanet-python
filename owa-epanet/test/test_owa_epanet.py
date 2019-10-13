2f6cc9f070735659f284e5faba578989b491ca8e
Ernesto Arandia <ernesto.arandia@xyleminc.com>
2019-10-13 09:56:19 -0400

    adds clean_dir

--- owa-epanet/test/test_owa_epanet.py
+++ owa-epanet/test/test_owa_epanet.py
@@ -14,6 +14,15 @@ wq_times = [86100, 85800, 85500, 85200, 84900, 84600, 84300, 84000, 83700, 83400
             72600, 72300, 72000, 71700, 71400, 71100, 70800, 70500, 70200, 69900, 69600, 69300, 69000, 68700, 68400]
 
 
+def clean_dir():
+    if os.path.exists('report.rpt'):
+        os.remove('report.rpt')
+    if os.path.exists('output.out'):
+        os.remove('output.out')
+    if os.path.exists('saved_inp_file.inp'):
+        os.remove('saved_inp_file.inp')
+
+
 def test_create_project():
     epanet_proj = en.createproject()
     assert(str(type(epanet_proj)) == '<class \'SwigPyObject\'>')
@@ -22,29 +31,20 @@ def test_create_project():
 def test_open():
     epanet_proj = en.createproject()
     en.open(ph=epanet_proj, inpFile=example_1_path, rptFile='report.rpt', outFile='output.out')
-    try:
-        os.remove('report.rpt')
-    except OSError:
-        pass
+    clean_dir()
 
 
 def test_close():
     epanet_proj = en.createproject()
     en.open(ph=epanet_proj, inpFile=example_1_path, rptFile='report.rpt', outFile='output.out')
     en.close(ph=epanet_proj)
-    try:
-        os.remove('report.rpt')
-    except OSError:
-        pass
+    clean_dir()
 
 
 def test_run_project():
     epanet_proj = en.createproject()
     en.runproject(ph=epanet_proj, inpFile=example_1_path, rptFile='report.rpt', outFile='output.out', pviewprog=None)
-    try:
-        os.remove('output.out')
-    except OSError:
-        pass
+    clean_dir()
 
 
 def test_get_count():
@@ -63,10 +63,7 @@ def test_get_count():
     num_contr = en.getcount(ph=epanet_proj, object=en.CONTROLCOUNT)
     assert (num_contr == 2)
     en.close(ph=epanet_proj)
-    try:
-        os.remove('report.rpt')
-    except OSError:
-        pass
+    clean_dir()
 
 
 def test_save_inp_file():
@@ -74,22 +71,16 @@ def test_save_inp_file():
     en.open(ph=epanet_proj, inpFile=example_1_path, rptFile='report.rpt', outFile='output.out')
     en.saveinpfile(ph=epanet_proj, filename='saved_inp_file.inp')
     assert(os.path.isfile('saved_inp_file.inp'))
-    try:
-        os.remove('saved_inp_file.inp')
-    except OSError:
-        pass
+    clean_dir()
 
 
 def test_openh():
     epanet_proj = en.createproject()
     en.open(ph=epanet_proj, inpFile=example_1_path, rptFile='report.rpt', outFile='output.out')
     res = en.openH(ph=epanet_proj)
-    assert(not res)
     en.close(ph=epanet_proj)
-    try:
-        os.remove('report.rpt')
-    except OSError:
-        pass
+    assert (not res)
+    clean_dir()
 
 
 def test_inith():
@@ -97,8 +88,9 @@ def test_inith():
     en.open(ph=epanet_proj, inpFile=example_1_path, rptFile='report.rpt', outFile='output.out')
     en.openH(ph=epanet_proj)
     res = en.initH(ph=epanet_proj, initFlag=0)
-    assert(not res)
     en.close(ph=epanet_proj)
+    assert (not res)
+    clean_dir()
 
 
 def test_runh():
@@ -107,8 +99,9 @@ def test_runh():
     en.openH(ph=epanet_proj)
     en.initH(ph=epanet_proj, initFlag=0)
     res = en.runH(ph=epanet_proj)
-    assert(res == 0)
     en.close(ph=epanet_proj)
+    assert (res == 0)
+    clean_dir()
 
 
 def test_nexth():
@@ -118,8 +111,9 @@ def test_nexth():
     en.initH(ph=epanet_proj, initFlag=0)
     en.runH(ph=epanet_proj)
     res = en.nextH(ph=epanet_proj)
-    assert(res == 3600)
     en.close(ph=epanet_proj)
+    assert (res == 3600)
+    clean_dir()
 
 
 def test_closeh():
@@ -128,8 +122,9 @@ def test_closeh():
     en.openH(ph=epanet_proj)
     en.initH(ph=epanet_proj, initFlag=0)
     res = en.closeH(ph=epanet_proj)
-    assert (not res)
     en.close(ph=epanet_proj)
+    assert (not res)
+    clean_dir()
 
 
 def test_inith_runh_nexth():
@@ -144,9 +139,10 @@ def test_inith_runh_nexth():
         tlist.append(t)
         if t <= 0:
             break
-    assert(tlist == timesteps)
     en.closeH(ph=epanet_proj)
     en.close(ph=epanet_proj)
+    assert (tlist == timesteps)
+    clean_dir()
 
 
 def test_solveh_solveq():
@@ -157,10 +153,7 @@ def test_solveh_solveq():
     en.report(ph=epanet_proj)
     en.close(ph=epanet_proj)
     assert (os.path.isfile('output.out'))
-    try:
-        os.remove('output.out')
-    except OSError:
-        pass
+    clean_dir()
 
 
 def test_initq_runq_nextq():
@@ -180,14 +173,7 @@ def test_initq_runq_nextq():
     en.report(ph=epanet_proj)
     en.close(ph=epanet_proj)
     assert(tlist == timesteps)
-    try:
-        os.remove('output.out')
-    except OSError:
-        pass
-    try:
-        os.remove('report.rpt')
-    except OSError:
-        pass
+    clean_dir()
 
 
 def test_stepq():
@@ -205,23 +191,16 @@ def test_stepq():
             break
     en.closeQ(ph=epanet_proj)
     en.close(ph=epanet_proj)
-    assert(tlist[:60] == wq_times)
-    try:
-        os.remove('output.out')
-    except OSError:
-        pass
-    try:
-        os.remove('report.rpt')
-    except OSError:
-        pass
+    clean_dir()
 
 
 def test_getqualtype():
     epanet_proj = en.createproject()
     en.open(ph=epanet_proj, inpFile=example_1_path, rptFile='report.rpt', outFile='output.out')
     qual_type = en.getqualtype(ph=epanet_proj)
-    assert qual_type == [1, 0]
     en.close(ph=epanet_proj)
+    assert qual_type == [1, 0]
+    clean_dir()
 
 
 def test_set_qualtype():
@@ -229,8 +208,9 @@ def test_set_qualtype():
     en.open(ph=epanet_proj, inpFile=example_1_path, rptFile='report.rpt', outFile='output.out')
     en.setqualtype(ph=epanet_proj, qualType=2, chemName='Age', chemUnits='seconds', traceNode=None)
     qual_type = en.getqualtype(ph=epanet_proj)
-    assert qual_type == [2, 0]
     en.close(ph=epanet_proj)
+    assert qual_type == [2, 0]
+    clean_dir()
 
 
 def test_water_age_sim():
@@ -252,11 +232,12 @@ def test_water_age_sim():
         age_list.append(node_age_list)
         if t <= 0:
             break
-    assert (age_list[26] == [1.0, 2.2141675704376946, 12.939125434025273, 24.44152992466322, 13.174235412569542,
-                             24.441519659540887, 15.679376648181817, 21.97064181429266, 19.048343501261524, 1.0])
     en.closeQ(ph=epanet_proj)
     en.closeH(ph=epanet_proj)
     en.close(ph=epanet_proj)
+    assert (age_list[26] == [1.0, 2.2141675704376946, 12.939125434025273, 24.44152992466322, 13.174235412569542,
+                             24.441519659540887, 15.679376648181817, 21.97064181429266, 19.048343501261524, 1.0])
+    clean_dir()
 
 
 # this test is failing due to a seg fault
@@ -266,6 +247,7 @@ def _test_add_node():
     en.addnode(ph=epanet_proj, id='33', nodeType=en.JUNCTION)
     num_nodes = en.getcount(ph=epanet_proj, object=en.NODECOUNT)
     assert num_nodes == 12
+    clean_dir()
 
 
 def test_getnodeindex():
@@ -276,6 +258,7 @@ def test_getnodeindex():
     for node_id in node_ids:
         node_idx.append(en.getnodeindex(ph=epanet_proj, id=node_id))
     assert node_idx == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    clean_dir()
 
 
 def test_getnodeid():
@@ -286,6 +269,7 @@ def test_getnodeid():
     for nidx in node_idx:
         node_id.append(en.getnodeid(ph=epanet_proj, index=nidx))
     assert node_id == ['10', '11', '12', '13', '21', '22', '23', '31', '32', '9', '2']
+    clean_dir()
 
 
 def test_add_link():
@@ -294,6 +278,7 @@ def test_add_link():
     en.addlink(ph=epanet_proj, id='L123', linkType=en.PIPE, fromNode='10', toNode='12')
     num_links = en.getcount(ph=epanet_proj, object=en.LINKCOUNT)
     assert num_links == 14
+    clean_dir()
 
 
 def test_setnodevalue():
@@ -325,9 +310,6 @@ def test_setnodevalue():
         initqual = en.getnodevalue(ph=epanet_proj, index=node_ind, property=en.INITQUAL)
         en.setnodevalue(ph=epanet_proj, index=node_ind, property=en.INITQUAL, value=initqual + 0.1)
         initqual_list.append(en.getnodevalue(ph=epanet_proj, index=node_ind, property=en.INITQUAL))
-        # sourcequal
-        # sourcepat
-        # sourcetype
         # tanklevel
         if en.getnodetype(ph=epanet_proj, index=node_ind) == en.TANK:
             tank_level = en.getnodevalue(ph=epanet_proj, index=node_ind, property=en.TANKLEVEL)
@@ -339,12 +321,6 @@ def test_setnodevalue():
     assert emitter_list == [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.0, 0.0]
     assert initqual_list == [0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 1.1, 1.1]
     assert tank_level_list ==[121.0]
-    try:
-        os.remove('output.out')
-    except OSError:
-        pass
-    try:
-        os.remove('report.rpt')
-    except OSError:
-        pass
+    clean_dir()
+
 
