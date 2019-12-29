542123c08bc5497bbacbee11c8770e08d8362664
Masud Rana <sm.masudrana@gmail.com>
2019-12-28 21:45:35 -0500

    changed .lib to .dll

--- owa-epanet/build_owa-epanet.bat
+++ owa-epanet/build_owa-epanet.bat
@@ -6,6 +6,10 @@ SET COMPILE_PATH=%Build_PATH%\build\
 
 MKDIR "%COMPILE_PATH%"
 CD "%COMPILE_PATH%"
+rem Building 64-bit toolkit for 64-bit python.
+rem If 32-bit toolkit is required ensure 32-bit python is located 
+rem by cmake (check CmakeList.txt) and replace '%CMAKE_PATH% ../ -A x64'
+rem with '%CMAKE_PATH% ../ -A Win32'."
 %CMAKE_PATH% ../ -A x64
 %CMAKE_PATH% --build . --config Release
 
