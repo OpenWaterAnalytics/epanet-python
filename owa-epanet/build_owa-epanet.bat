7fa115b8a6fb82516612376995a5a5b2ffda8a31
Masud Rana <sm.masudrana@gmail.com>
2020-01-02 22:27:43 -0500

    Downloading the submodule properly.

--- owa-epanet/build_owa-epanet.bat
+++ owa-epanet/build_owa-epanet.bat
@@ -1,6 +1,6 @@
 cls
 
-git clone --branch=dev https://github.com/OpenWaterAnalytics/EPANET.git
+git submodule update --init
 
 SET CMAKE_PATH=cmake.exe 
 SET Build_PATH=%CD%
@@ -14,6 +14,8 @@ rem by cmake (check CmakeList.txt) and replace '%CMAKE_PATH% ../ -A x64'
 rem with '%CMAKE_PATH% ../ -A Win32'."
 %CMAKE_PATH% ../ -A x64
 %CMAKE_PATH% --build . --config Release
+
+rem 'After successful compilation "toolkit.py", "_toolkit.pyd", "epanet2.dll" will be created. You can start using the toolkit by importing the module "toolkit.py".
 
 cd ..
 pause
