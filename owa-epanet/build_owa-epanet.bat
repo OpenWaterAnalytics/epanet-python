f5a606f73115c9a7778e8e47f3705ff45b4c5b8c
Masud Rana <sm.masudrana@gmail.com>
2020-01-04 12:27:06 -0500

    Updated Readme.md and library paths.

--- owa-epanet/build_owa-epanet.bat
+++ owa-epanet/build_owa-epanet.bat
@@ -1,18 +1,20 @@
 cls
 
+rem Bulding the toolkit package for Windows. The preferred way to build the package is by running 'python setup.py sdist bdist_wheel'.
+
 git submodule update --init
 
-SET CMAKE_PATH=cmake.exe 
+SET CMAKE_PATH=cmake.exe
 SET Build_PATH=%CD%
-SET COMPILE_PATH=%Build_PATH%\build\
+SET COMPILE_PATH=%Build_PATH%\_cmake_build\windows\cmake-build
 
 MKDIR "%COMPILE_PATH%"
 CD "%COMPILE_PATH%"
 rem Building 64-bit toolkit for 64-bit python.
-rem If 32-bit toolkit is required ensure 32-bit python is located 
+rem If 32-bit toolkit is required ensure 32-bit python is located
 rem by cmake (check CmakeList.txt) and replace '%CMAKE_PATH% ../ -A x64'
 rem with '%CMAKE_PATH% ../ -A Win32'."
-%CMAKE_PATH% ../ -A x64
+%CMAKE_PATH% ../../../ -A x64
 %CMAKE_PATH% --build . --config Release
 
 rem 'After successful compilation "toolkit.py", "_toolkit.pyd", "epanet2.dll" will be created. You can start using the toolkit by importing the module "toolkit.py".
