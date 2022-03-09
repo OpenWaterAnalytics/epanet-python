bef8fe2893355de5146c00be896b5c450b1cf274
Sam Rensenhouse <sam.rensenhouse@xyleminc.com>
2022-03-09 14:15:02 -0500

    changing test to allow for floating point differences

--- owa-epanet/test/test_owa_epanet.py
+++ owa-epanet/test/test_owa_epanet.py
@@ -236,8 +236,9 @@ def test_water_age_sim():
     en.closeQ(ph=epanet_proj)
     en.closeH(ph=epanet_proj)
     en.close(ph=epanet_proj)
-    assert age_list[26] == [1.0, 2.2141675704376946, 12.939125434025273, 24.44152992466322, 13.174235412569542,
-                            24.441519659540887, 15.679376648181817, 21.97064181429266, 19.048343501261524, 1.0]
+    assert age_list[26] == pytest.approx(
+        [1.0, 2.2141675704376946, 12.939125434025273, 24.44152992466322, 13.174235412569542,
+         24.441519659540887, 15.679376648181817, 21.97064181429266, 19.048343501261524, 1.0])
     clean_dir()
 
 
@@ -324,6 +325,7 @@ def test_setnodevalue():
     assert tank_level_list ==[121.0]
     clean_dir()
 
+
 def test_setcurve():
     def make_array(values):
         dbl_arr = en.doubleArray(len(values))
@@ -340,6 +342,7 @@ def test_setcurve():
     en.setcurve(ph=epanet_proj, index=curve_index, xValues=xvalues, yValues=yvalues, nPoints=5)
     count = en.getcurvelen(ph=epanet_proj, index=curve_index)
     assert count == 5
+
 
 def test_coords():
     epanet_proj = en.createproject()
